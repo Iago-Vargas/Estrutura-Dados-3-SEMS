eda604582de3772ef137b67f21d99515f3a6798f
Iago-Vargas <166236052+Iago-Vargas@users.noreply.github.com>
2024-04-26 09:07:22 -0300

    Update main.py

--- Avaliação/main.py
+++ Avaliação/main.py
@@ -2,7 +2,7 @@ import random
 
 def gerar_listas(Listas, Tamanho):
     listas = []
-    for _ in range(N):
+    for _ in range(Listas):
         lista = [random.randint(1, 100) for _ in range(Tamanho)]
         listas.append(lista)
     return listas
@@ -18,19 +18,20 @@ def numeros_que_aparecem(listas):
     return numeros
 
 def numeros_que_aparecem_em_apenas_uma(lista_generica):
-    numeros_repetidos = set()
-    numeros_unicos = set()
+    todos_numeros = set()
+    unicos = set()
+    repetidos = set()
+
     for lista in lista_generica:
         for numero in lista:
-            if numero in numeros_repetidos:
-                numeros_unicos.discard(numero)
+            if numero in todos_numeros:
+                unicos.discard(numero)
+                repetidos.add(numero)
             else:
-                if numero in numeros_unicos:
-                    numeros_unicos.remove(numero)
-                else:
-                    numeros_unicos.add(numero)
-                    numeros_repetidos.add(numero)
-    return numeros_unicos
+                todos_numeros.add(numero)
+                unicos.add(numero)
+
+    return unicos - repetidos
 
 Listas = 1000  # Número de listas
 Tamanho = 500  # Tamanho das listas
@@ -44,5 +45,8 @@ print("\nNÚMEROS QUE APARECEM NAS LISTAS:")
 print(numeros_que_aparecem(listas))
 
 print("\nNÚMEROS QUE APARECEM EM APENAS UMA LISTA:")
-lista_generica = [[6], [2, 6, 8, 10], [4, 6, 8], [6, 8], [1, 3, 5]]
-print(numeros_que_aparecem_em_apenas_uma(lista_generica))
+valores_unicos = numeros_que_aparecem_em_apenas_uma(listas)
+if valores_unicos:
+    print(valores_unicos)
+else:
+    print("0")
